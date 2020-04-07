0c9b931ea1e62b4e16c325c23428264b01debbc5
pushpindersingh1811 <61315889+pushpindersingh1811@users.noreply.github.com>
2020-04-07 12:07:31 +0530

    Face And Eye Detection

---
+++ face_detection.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+
+#img = cv2.imread('grp.jpg')
+
+cap = cv2.VideoCapture(0)
+
+while (cap.isOpened()):
+
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.2, 3)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 3)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_color)
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 3)
+
+    cv2.imshow('face', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
